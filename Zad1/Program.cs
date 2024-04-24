cd30550304515099a7573c5c817e602b4cfe3e53
Viktoria Hristova <Viktoria Hristova@DESKTOP-7D09794>
2024-04-24 11:14:21 +0300

    Add project files.

---
+++ Zad1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle = new Circle(5);
+            Console.WriteLine("Лицето на кръга: " + circle.CalculateArea());
+
+          
+            Cylinder cylinder = new Cylinder(3, 7);
+            Console.WriteLine("Обема на цилиндъра: " + cylinder.CalculateVolume());
+
+            
+            Cone cone = new Cone(4, 6);
+            Console.WriteLine("Обема на конуса: " + cone.CalculateVolume());
+
+        }
+    }
+
+
+    class Circle
+    {
+        protected double radius;
+
+        // Конструктор по подразбиране
+        public Circle()
+        {
+            radius = 0;
+        }
+
+        // Конструктор за инициализация на радиуса
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+
+    class Cylinder : Circle
+    {
+        protected double height;
+
+        // Конструктор по подразбиране
+        public Cylinder() : base()
+        {
+            height = 0;
+        }
+
+        
+        public Cylinder(double r, double h) : base(r)
+        {
+            height = h;
+        }
+
+        
+        public double CalculateVolume()
+        {
+            return base.CalculateArea() * height;
+        }
+    }
+
+
+    class Cone : Circle
+    {
+        protected double height;
+
+        
+        public Cone() : base()
+        {
+            height = 0;
+        }
+
+        
+        public Cone(double r, double h) : base(r)
+        {
+            height = h;
+        }
+
+        
+        public double CalculateVolume()
+        {
+            return (1.0 / 3.0) * base.CalculateArea() * height;
+        }
+    }
+
+
+}
---
+++ Zad2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person = new Person("John Doe", "1234567890", "ID123456");
+            Console.WriteLine("Person:");
+            person.Output();
+            Console.WriteLine();
+
+            Student student = new Student("Jane Smith", "0987654321", "ID654321", "Computer Science", "Group A", "123456");
+            Console.WriteLine("Student:");
+            student.Output();
+
+        }
+    }
+
+
+
+    class Person
+    {
+        protected string name;
+        protected string egn;
+        protected string idCard;
+
+       
+        public Person()
+        {
+            name = "";
+            egn = "";
+            idCard = "";
+        }
+
+        
+        public Person(string n, string e, string id)
+        {
+            name = n;
+            egn = e;
+            idCard = id;
+        }
+
+       
+        public virtual void Output()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("EGN: " + egn);
+            Console.WriteLine("ID Card: " + idCard);
+        }
+    }
+
+    
+    class Student : Person
+    {
+        protected string specialty;
+        protected string group;
+        protected string facultyNumber;
+
+       
+        public Student() : base()
+        {
+            specialty = "";
+            group = "";
+            facultyNumber = "";
+        }
+
+        
+        public Student(string n, string e, string id, string spec, string grp, string facNum) : base(n, e, id)
+        {
+            specialty = spec;
+            group = grp;
+            facultyNumber = facNum;
+        }
+
+        
+        public override void Output()
+        {
+            base.Output();
+            Console.WriteLine("Specialty: " + specialty);
+            Console.WriteLine("Group: " + group);
+            Console.WriteLine("Faculty Number: " + facultyNumber);
+        }
+    }
+
+}
